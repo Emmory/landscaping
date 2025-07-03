4761d59cf353d1085c0ef552dc3bc60cf4392bb6
Emmory Carias Gonzalez <47765899+Emmory@users.noreply.github.com>
2025-07-03 13:13:00 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 
+const swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+    1400: { slidesPerView: 4 }
+  },
+  slidesPerView: 1,
+});
